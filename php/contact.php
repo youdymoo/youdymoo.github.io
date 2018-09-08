e08e5d13c3fffe62eb4636f91f52b074dfb1d2db
Ray Cao <youdymoo@umich.edu>
2018-09-07 23:09:57 -0400

    major changes

--- php/contact.php
+++ php/contact.php
@@ -73,7 +73,7 @@ if(mail($address, $e_subject, $msg, $headers)) {
 	echo "<fieldset>";
 	echo "<div id='success_page'>";
 	echo "<h3>Email Sent Successfully.</h3>";
-	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
+	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to Ray.</p>";
 	echo "</div>";
 	echo "</fieldset>";
 
